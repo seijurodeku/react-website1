79d8a53be25fe42c3edf14d1c1d12573f9eef16c
Dave McFarland <dave@dmcfarland.com>
2019-05-06 13:48:33 -0400

    Updates

---
+++ precache-manifest.3972b9384b8d7be8aa69e4e7ef565bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87200f3e84b05fb454e86887766556a8",
+    "url": "/react-website1/index.html"
+  },
+  {
+    "revision": "ca410477cd84ea52d0ec",
+    "url": "/react-website1/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-website1/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ca410477cd84ea52d0ec",
+    "url": "/react-website1/static/js/main.e05e392b.chunk.js"
+  },
+  {
+    "revision": "ed04a2504f63da5ce00c",
+    "url": "/react-website1/static/js/runtime~main.e294a54d.js"
+  }
+]);
